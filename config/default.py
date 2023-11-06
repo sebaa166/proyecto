2c757dcebc051a0f050ab7113af98cf7246629e2
seba <seba16638@gmail.com>
2023-11-06 16:42:44 -0300

    hu

--- config/default.py
+++ config/default.py
@@ -31,8 +31,8 @@ APP_ENV = ''
 
 MAIL_SERVER = os.getenv('MAIL_SERVER_ENV')
 MAIL_PORT = 587
-MAIL_USERNAME = 'tu correo'
-MAIL_PASSWORD = 'tu contraseña'
+MAIL_USERNAME = os.getenv('MAIL_USERNAME_ENV')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD_ENV')
 DONT_REPLY_FROM_EMAIL = 'dirección from'
 ADMINS = ('juanjo@j2logo.com', )
 MAIL_USE_TLS = True
